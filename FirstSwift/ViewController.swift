fda5d1087c0456bc7d85d2161cfab81b38735a69
Jian Ming Zhang <smartzjm@gmail.com>
2015-08-07 11:48:50 +0800

    Initial Commit

---
+++ FirstSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstSwift
+//
+//  Created by iStig on 15/8/7.
+//  Copyright (c) 2015年 iStig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
